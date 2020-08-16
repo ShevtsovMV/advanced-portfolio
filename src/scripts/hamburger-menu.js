5a0f140db8bb0278f973fc29b0fec2855311387a
Maxim Shevtsov <shevtsov.well@gmail.com>
2020-08-16 18:51:21 +0300

    Изменения в функционале гамбургер-меню

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -1,14 +1,19 @@
-let burger = document.querySelector('.hamburger-menu');
-let overlay = document.querySelector('.header__menu');
-let links = overlay.querySelectorAll('.menu__link');
+const burger = document.querySelector('.hamburger-menu');
+const overlay = document.querySelector('.header__menu');
+const links = overlay.querySelectorAll('.menu__link');
 
-const toggleMenu = () => {
+const toggleClassMenu = () => {
     burger.classList.toggle('hamburger-menu_active');
     overlay.classList.toggle('header__menu_active');
-}
+};
+
+const removeClassMenu = () => {
+    burger.classList.remove('hamburger-menu_active');
+    overlay.classList.remove('header__menu_active');
+};
 
 links.forEach(function(e) {
-    e.addEventListener('click', toggleMenu);
+    e.addEventListener('click', removeClassMenu);
 })
 
-burger.addEventListener('click', toggleMenu);
+burger.addEventListener('click', toggleClassMenu);
