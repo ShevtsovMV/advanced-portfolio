5b32e1994c5a1412eff77eaff15dd66faf6f173f
Maxim Shevtsov <shevtsov.well@gmail.com>
2020-08-23 12:09:40 +0300

    Реализован компонент добавления тегов tagsAdder

---
+++ src/admin/components/tagsAdder/tagsAdder.stories.js
@@ -0,0 +1,25 @@
+import tagsAdder from "./tagsAdder.vue";
+
+export default {
+  title: "tagsAdder",
+  component: { tagsAdder },
+};
+
+export const defaultView = () => ({
+  components: { tagsAdder },
+  data() {
+    return {
+      tags: "One, two, three"
+    };
+  },
+  template: `
+    <div>
+      <pre>{{tags}}</pre>  
+      <tags-adder v-model="tags" />
+    </div>
+  `,
+});
+
+defaultView.story = {
+  name: "Стандартный вид",
+};
