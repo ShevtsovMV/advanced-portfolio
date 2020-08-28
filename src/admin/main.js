7fb357a8c097e3f7b9a0f23c1ff8f3b9de959a12
Maxim Shevtsov <shevtsov.well@gmail.com>
2020-08-28 20:33:50 +0300

    Реализован логин и получение токена c сохранением его в localStorage

--- src/admin/main.js
+++ src/admin/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import SimpleVueValidator from "simple-vue-validator";
+
+Vue.use(SimpleVueValidator, {
+  mode: "manual"
+});
 
 new Vue({
   el: "#app-root",
